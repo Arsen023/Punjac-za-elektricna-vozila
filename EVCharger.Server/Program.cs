42fcd9d12626c0cb7153f64392428c16028de52b
Ddavidd123 <stosicdavid6@gmail.com>
2026-05-10 12:29:11 +0200

    rad na serveru

--- EVCharger.Server/Program.cs
+++ EVCharger.Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace EVCharger.Server
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(EVChargerService));
+
+            try
+            {
+                host.Open();
+
+                Console.WriteLine("EV Charger WCF service is running...");
+                Console.WriteLine("Address: net.tcp://localhost:4000/EVChargerService");
+                Console.WriteLine("Press ENTER to stop server.");
+
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Server error: " + ex.Message);
+                host.Abort();
+            }
         }
     }
 }
